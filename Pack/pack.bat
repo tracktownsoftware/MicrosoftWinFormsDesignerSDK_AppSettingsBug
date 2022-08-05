9149b086a9327209b59ff31923f62eb002695269
Chris Hamilton <chris@tracktownsoftware.com>
2022-08-05 09:22:05 -0700

    Corrected name of solution in build.bat

--- Pack/pack.bat
+++ Pack/pack.bat
@@ -1,5 +1,5 @@
 set PACKAGEVERSION=1.0.102
-msbuild ..\MicrosoftWinFormsDesignerSDK_Test1.sln -p:Configuration=Release 
+msbuild ..\MicrosoftWinFormsDesignerSDK_AppSettingsBug.sln -p:Configuration=Release 
 
 rem build MyButtonLibrary package
 dotnet pack -p:PackageVersion=%PACKAGEVERSION% ..\MyButtonLibrary\MyButtonLibrary.csproj --configuration=Release --output Packages
